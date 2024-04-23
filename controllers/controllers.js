8ca07aaba82eb22e23300d3ef4e7aad28cc29775
Your Name <you@example.com>
2024-04-23 14:19:05 -0500

    55%

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,6 +1,8 @@
 //const cuerpo = require('../DB/cuerpo');
 const db = require("../sql_table/db")
 exports.inicio = async (req, res) => {
+  const datos=db.query("select * from planMovil_Estudiante")
+  console.log(datos);
   try {
     if(req.cookies.nombre){
       res.redirect(302,"/home") 
